c917d0b2271e19940134d0351bcec29b136beab4
Alexander, Antony <alexander_anto@outlook.com>
2021-07-21 11:10:58 -0400

    initial commit

---
+++ src/test/java/com/selenium/maven/AppiumTest.java
@@ -0,0 +1,5 @@
+package com.selenium.maven;
+
+public class AppiumTest {
+
+}
---
+++ src/test/java/com/selenium/maven/RestApiTest.java
@@ -0,0 +1,17 @@
+package com.selenium.maven;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+	@Test
+	public void postJira() {
+		System.out.println("Post JIRA");
+	}
+	
+	@Test
+	public void runApi() {
+		System.out.println("Run API");
+	}
+	
+}
