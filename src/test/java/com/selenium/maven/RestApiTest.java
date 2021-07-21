53a2a2edc2110ff6940f631e979c147763a84a1d
Alexander, Antony <alexander_anto@outlook.com>
2021-07-21 12:00:55 -0400

    setup maven profile

--- src/test/java/com/selenium/maven/RestApiTest.java
+++ src/test/java/com/selenium/maven/RestApiTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class RestApiTest {
 
-	@Test
+	@Test(enabled=false)
 	public void postJira() {
 		System.out.println("Post JIRA");
 	}
--- src/test/java/com/selenium/maven/SeleniumTest.java
+++ src/test/java/com/selenium/maven/SeleniumTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
-	@Test
+	@Test(dependsOnMethods= {"elementsUI"})
 	public void browserAutomation() {
 		System.out.println("Browser Automation!");
 	}
